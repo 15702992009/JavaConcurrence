260baa2ebb0a001bd09c748e9d5ddd30da362698
kylinToDo <jaybo_x@163.com>
2020-03-18 17:03:17 +0800

    end

---
+++ src/chapter8/ExecutorsOOM.java
@@ -0,0 +1,26 @@
+package chapter8;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ExecutorsOOM {
+    private static ExecutorService executor = Executors.newFixedThreadPool(15);
+//    ThreadPoolExecutor
+    public static void main(String[] args) {
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            executor.execute(new SubThread());
+        }
+    }
+}
+
+class SubThread implements Runnable {
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            //do nothing
+        }
+    }
+}
